de392781b2cbfbc23c24241d98e39347b5b24cec
951237 <44091528+951237@users.noreply.github.com>
2020-12-03 02:14:29 +0900

    Update 201203_TIL_load excel file.py
    - 엑셀 파일 불러오기
    - 엑셀의 내용 화면에 출력해보기

--- 201203_TIL_load excel file.py
+++ 201203_TIL_load excel file.py
@@ -5,8 +5,8 @@ wb = load_workbook("sample.xlsx")
 ws = wb.active
 
 # 셀의 내용 화면에 찍어보기
-for x in range(1, ws.max_row + 1):
-    for y in range(1, ws.max_column + 1):
+for x in range(1, ws.max_row + 1):  # 마지막 행까지
+    for y in range(1, ws.max_column + 1):   # 마지마 칼럼까지
         print(ws.cell(row=x, column = y).value, end = '')
 
     print()
